cc4abbc2ea3f1467bfc3c7e9df03e9c9b69266e5
Ryne <rheron93@gmail.com>
2018-10-25 23:24:48 -0500

    -Added color functionality -Removed text cursor from color boxes -Changed cursor of those color boxes to a hand -Added a box to display the current selected color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -75,6 +75,7 @@
             this.textBox28 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.drawPanel = new System.Windows.Forms.PictureBox();
+            this.Chosen_Color_Display = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.menuBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.drawPanel)).BeginInit();
@@ -150,6 +151,7 @@
             // menuBar
             // 
             this.menuBar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.menuBar.Controls.Add(this.Chosen_Color_Display);
             this.menuBar.Controls.Add(this.eraserComboBox);
             this.menuBar.Controls.Add(this.penComboBox);
             this.menuBar.Controls.Add(this.pencilComboBox);
@@ -276,254 +278,338 @@
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.Red;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox2.Location = new System.Drawing.Point(74, 50);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(25, 25);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.Yellow;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox3.Location = new System.Drawing.Point(105, 50);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(25, 25);
             this.textBox3.TabIndex = 4;
+            this.textBox3.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox4
             // 
             this.textBox4.BackColor = System.Drawing.Color.Lime;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox4.Location = new System.Drawing.Point(136, 50);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(25, 25);
             this.textBox4.TabIndex = 5;
+            this.textBox4.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox5
             // 
             this.textBox5.BackColor = System.Drawing.Color.Aqua;
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox5.Location = new System.Drawing.Point(167, 50);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(25, 25);
             this.textBox5.TabIndex = 6;
+            this.textBox5.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox6
             // 
             this.textBox6.BackColor = System.Drawing.Color.Blue;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox6.Location = new System.Drawing.Point(198, 50);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(25, 25);
             this.textBox6.TabIndex = 7;
+            this.textBox6.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox7
             // 
             this.textBox7.BackColor = System.Drawing.Color.Fuchsia;
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox7.Location = new System.Drawing.Point(229, 50);
             this.textBox7.Multiline = true;
             this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
             this.textBox7.Size = new System.Drawing.Size(25, 25);
             this.textBox7.TabIndex = 8;
+            this.textBox7.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox8
             // 
             this.textBox8.BackColor = System.Drawing.Color.LemonChiffon;
+            this.textBox8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox8.Location = new System.Drawing.Point(260, 50);
             this.textBox8.Multiline = true;
             this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
             this.textBox8.Size = new System.Drawing.Size(25, 25);
             this.textBox8.TabIndex = 9;
+            this.textBox8.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox9
             // 
             this.textBox9.BackColor = System.Drawing.Color.SpringGreen;
+            this.textBox9.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox9.Location = new System.Drawing.Point(291, 50);
             this.textBox9.Multiline = true;
             this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
             this.textBox9.Size = new System.Drawing.Size(25, 25);
             this.textBox9.TabIndex = 10;
+            this.textBox9.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox10
             // 
             this.textBox10.BackColor = System.Drawing.Color.PowderBlue;
+            this.textBox10.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox10.Location = new System.Drawing.Point(322, 50);
             this.textBox10.Multiline = true;
             this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
             this.textBox10.Size = new System.Drawing.Size(25, 25);
             this.textBox10.TabIndex = 11;
+            this.textBox10.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox11
             // 
             this.textBox11.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox11.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox11.Location = new System.Drawing.Point(353, 50);
             this.textBox11.Multiline = true;
             this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
             this.textBox11.Size = new System.Drawing.Size(25, 25);
             this.textBox11.TabIndex = 12;
+            this.textBox11.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox12
             // 
             this.textBox12.BackColor = System.Drawing.Color.DeepPink;
+            this.textBox12.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox12.Location = new System.Drawing.Point(384, 50);
             this.textBox12.Multiline = true;
             this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
             this.textBox12.Size = new System.Drawing.Size(25, 25);
             this.textBox12.TabIndex = 13;
+            this.textBox12.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox13
             // 
             this.textBox13.BackColor = System.Drawing.Color.SandyBrown;
+            this.textBox13.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox13.Location = new System.Drawing.Point(415, 50);
             this.textBox13.Multiline = true;
             this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
             this.textBox13.Size = new System.Drawing.Size(25, 25);
             this.textBox13.TabIndex = 14;
+            this.textBox13.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox14
             // 
+            this.textBox14.BackColor = System.Drawing.Color.White;
+            this.textBox14.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox14.Location = new System.Drawing.Point(12, 50);
             this.textBox14.Multiline = true;
             this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
             this.textBox14.Size = new System.Drawing.Size(25, 25);
             this.textBox14.TabIndex = 15;
+            this.textBox14.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox15
             // 
             this.textBox15.BackColor = System.Drawing.Color.Silver;
+            this.textBox15.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox15.Location = new System.Drawing.Point(43, 50);
             this.textBox15.Multiline = true;
             this.textBox15.Name = "textBox15";
+            this.textBox15.ReadOnly = true;
             this.textBox15.Size = new System.Drawing.Size(25, 25);
             this.textBox15.TabIndex = 16;
+            this.textBox15.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox16
             // 
             this.textBox16.BackColor = System.Drawing.Color.Sienna;
+            this.textBox16.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox16.Location = new System.Drawing.Point(415, 19);
             this.textBox16.Multiline = true;
             this.textBox16.Name = "textBox16";
+            this.textBox16.ReadOnly = true;
             this.textBox16.Size = new System.Drawing.Size(25, 25);
             this.textBox16.TabIndex = 17;
+            this.textBox16.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox17
             // 
             this.textBox17.BackColor = System.Drawing.Color.BlueViolet;
+            this.textBox17.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox17.Location = new System.Drawing.Point(384, 19);
             this.textBox17.Multiline = true;
             this.textBox17.Name = "textBox17";
+            this.textBox17.ReadOnly = true;
             this.textBox17.Size = new System.Drawing.Size(25, 25);
             this.textBox17.TabIndex = 18;
+            this.textBox17.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox18
             // 
             this.textBox18.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox18.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox18.Location = new System.Drawing.Point(353, 19);
             this.textBox18.Multiline = true;
             this.textBox18.Name = "textBox18";
+            this.textBox18.ReadOnly = true;
             this.textBox18.Size = new System.Drawing.Size(25, 25);
             this.textBox18.TabIndex = 19;
+            this.textBox18.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox19
             // 
             this.textBox19.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.textBox19.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox19.Location = new System.Drawing.Point(322, 19);
             this.textBox19.Multiline = true;
             this.textBox19.Name = "textBox19";
+            this.textBox19.ReadOnly = true;
             this.textBox19.Size = new System.Drawing.Size(25, 25);
             this.textBox19.TabIndex = 20;
+            this.textBox19.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox20
             // 
             this.textBox20.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.textBox20.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox20.Location = new System.Drawing.Point(291, 19);
             this.textBox20.Multiline = true;
             this.textBox20.Name = "textBox20";
+            this.textBox20.ReadOnly = true;
             this.textBox20.Size = new System.Drawing.Size(25, 25);
             this.textBox20.TabIndex = 21;
+            this.textBox20.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox21
             // 
             this.textBox21.BackColor = System.Drawing.Color.DarkKhaki;
+            this.textBox21.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox21.Location = new System.Drawing.Point(260, 19);
             this.textBox21.Multiline = true;
             this.textBox21.Name = "textBox21";
+            this.textBox21.ReadOnly = true;
             this.textBox21.Size = new System.Drawing.Size(25, 25);
             this.textBox21.TabIndex = 22;
+            this.textBox21.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox22
             // 
             this.textBox22.BackColor = System.Drawing.Color.Purple;
+            this.textBox22.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox22.Location = new System.Drawing.Point(229, 19);
             this.textBox22.Multiline = true;
             this.textBox22.Name = "textBox22";
+            this.textBox22.ReadOnly = true;
             this.textBox22.Size = new System.Drawing.Size(25, 25);
             this.textBox22.TabIndex = 23;
+            this.textBox22.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox23
             // 
             this.textBox23.BackColor = System.Drawing.Color.Navy;
+            this.textBox23.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox23.Location = new System.Drawing.Point(198, 19);
             this.textBox23.Multiline = true;
             this.textBox23.Name = "textBox23";
+            this.textBox23.ReadOnly = true;
             this.textBox23.Size = new System.Drawing.Size(25, 25);
             this.textBox23.TabIndex = 24;
+            this.textBox23.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox24
             // 
             this.textBox24.BackColor = System.Drawing.Color.Teal;
+            this.textBox24.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox24.Location = new System.Drawing.Point(167, 19);
             this.textBox24.Multiline = true;
             this.textBox24.Name = "textBox24";
+            this.textBox24.ReadOnly = true;
             this.textBox24.Size = new System.Drawing.Size(25, 25);
             this.textBox24.TabIndex = 25;
+            this.textBox24.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox25
             // 
             this.textBox25.BackColor = System.Drawing.Color.Green;
+            this.textBox25.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox25.Location = new System.Drawing.Point(136, 19);
             this.textBox25.Multiline = true;
             this.textBox25.Name = "textBox25";
+            this.textBox25.ReadOnly = true;
             this.textBox25.Size = new System.Drawing.Size(25, 25);
             this.textBox25.TabIndex = 26;
+            this.textBox25.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox26
             // 
             this.textBox26.BackColor = System.Drawing.Color.Olive;
+            this.textBox26.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox26.Location = new System.Drawing.Point(105, 19);
             this.textBox26.Multiline = true;
             this.textBox26.Name = "textBox26";
+            this.textBox26.ReadOnly = true;
             this.textBox26.Size = new System.Drawing.Size(25, 25);
             this.textBox26.TabIndex = 27;
+            this.textBox26.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox27
             // 
             this.textBox27.BackColor = System.Drawing.Color.Maroon;
+            this.textBox27.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox27.Location = new System.Drawing.Point(74, 19);
             this.textBox27.Multiline = true;
             this.textBox27.Name = "textBox27";
+            this.textBox27.ReadOnly = true;
             this.textBox27.Size = new System.Drawing.Size(25, 25);
             this.textBox27.TabIndex = 28;
+            this.textBox27.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox28
             // 
             this.textBox28.BackColor = System.Drawing.Color.Gray;
+            this.textBox28.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox28.Location = new System.Drawing.Point(43, 19);
             this.textBox28.Multiline = true;
             this.textBox28.Name = "textBox28";
+            this.textBox28.ReadOnly = true;
             this.textBox28.Size = new System.Drawing.Size(25, 25);
             this.textBox28.TabIndex = 29;
+            this.textBox28.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.textBox1.Location = new System.Drawing.Point(12, 19);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(25, 25);
             this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Click += new System.EventHandler(this.colorSelect_Click);
             // 
             // drawPanel
             // 
@@ -537,6 +623,15 @@
             this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseDown);
             this.drawPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseMove);
             this.drawPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseUp);
+            // 
+            // Chosen_Color_Display
+            // 
+            this.Chosen_Color_Display.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Chosen_Color_Display.Location = new System.Drawing.Point(136, 91);
+            this.Chosen_Color_Display.Name = "Chosen_Color_Display";
+            this.Chosen_Color_Display.ReadOnly = true;
+            this.Chosen_Color_Display.Size = new System.Drawing.Size(304, 20);
+            this.Chosen_Color_Display.TabIndex = 36;
             // 
             // Form1
             // 
@@ -611,6 +706,7 @@
         private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
         private System.Windows.Forms.PictureBox drawPanel;
+        private System.Windows.Forms.TextBox Chosen_Color_Display;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -13,23 +14,67 @@ namespace Assignment4
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-            g = drawPanel.CreateGraphics();
-        }
-
         Graphics g;
         bool startDraw = false;
         int? initX = null;
         int? initY = null;
         int width;
+        Color chosenColor = Color.Black;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = drawPanel.CreateGraphics();
+            Chosen_Color_Display.BackColor = chosenColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> pencilList = new List<string>() { "size 1", "size 2", "size 3" };
+            List<string> penList = new List<string>() { "size 5", "size 6", "size 7", "size 8" };
+            List<string> eraserList = new List<string>() { "size 1", "size 2", "size 3" };
+            pencilComboBox.DataSource = pencilList;
+            penComboBox.DataSource = penList;
+            eraserComboBox.DataSource = eraserList;
+
+            //All this just to make the cursor disappear
+            textBox1.GotFocus += Color_GotFocus;
+            textBox2.GotFocus += Color_GotFocus;
+            textBox3.GotFocus += Color_GotFocus;
+            textBox4.GotFocus += Color_GotFocus;
+            textBox5.GotFocus += Color_GotFocus;
+            textBox6.GotFocus += Color_GotFocus;
+            textBox7.GotFocus += Color_GotFocus;
+            textBox8.GotFocus += Color_GotFocus;
+            textBox9.GotFocus += Color_GotFocus;
+            textBox10.GotFocus += Color_GotFocus;
+            textBox11.GotFocus += Color_GotFocus;
+            textBox12.GotFocus += Color_GotFocus;
+            textBox13.GotFocus += Color_GotFocus;
+            textBox14.GotFocus += Color_GotFocus;
+            textBox15.GotFocus += Color_GotFocus;
+            textBox16.GotFocus += Color_GotFocus;
+            textBox17.GotFocus += Color_GotFocus;
+            textBox18.GotFocus += Color_GotFocus;
+            textBox19.GotFocus += Color_GotFocus;
+            textBox20.GotFocus += Color_GotFocus;
+            textBox21.GotFocus += Color_GotFocus;
+            textBox22.GotFocus += Color_GotFocus;
+            textBox23.GotFocus += Color_GotFocus;
+            textBox24.GotFocus += Color_GotFocus;
+            textBox25.GotFocus += Color_GotFocus;
+            textBox26.GotFocus += Color_GotFocus;
+            textBox27.GotFocus += Color_GotFocus;
+            textBox28.GotFocus += Color_GotFocus;
+
+            Chosen_Color_Display.GotFocus += Color_GotFocus;
+        }
 
         private void drawPanel_MouseMove(object sender, MouseEventArgs e)
         {
             if (startDraw)
             {
-                Pen p = new Pen(Color.Black, width);
+                Pen p = new Pen(chosenColor, width);
                 g.DrawLine(p, new Point(initX ?? e.X, initY ?? e.Y), new Point(e.X, e.Y));
                 initX = e.X;
                 initY = e.Y;
@@ -102,12 +147,12 @@ namespace Assignment4
 
             if ((string)penComboBox.SelectedItem == "size 6")
             {
-                width = 2;
+                width = 6;
             }
 
             if ((string)penComboBox.SelectedItem == "size 7")
             {
-                width = 3;
+                width = 7;
             }
 
             if ((string)penComboBox.SelectedItem == "size 8")
@@ -117,22 +162,31 @@ namespace Assignment4
 
         }
 
-
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void colorSelect_Click(object sender, EventArgs e)
         {
-            List<string> pencilList = new List<string>() { "size 1", "size 2", "size 3" };
-            List<string> penList = new List<string>() { "size 5", "size 6", "size 7", "size 8" };
-            List<string> eraserList = new List<string>() { "size 1", "size 2", "size 3" };
-            pencilComboBox.DataSource = pencilList;
-            penComboBox.DataSource = penList;
-            eraserComboBox.DataSource = eraserList;
-
+            if(sender as TextBox != null)
+            {
+                chosenColor = ((TextBox)sender).BackColor;
+                Chosen_Color_Display.BackColor = chosenColor;
+            }
         }
 
         private void penButton_Click(object sender, EventArgs e)
         {
             width = 5;
+        }
+
+
+
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+
+        private void Color_GotFocus(object sender, EventArgs e)
+        {
+            if (sender as TextBox != null)
+            {
+                HideCaret(((TextBox)sender).Handle);
+            }
         }
     }
 }
